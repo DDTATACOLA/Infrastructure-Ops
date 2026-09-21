d68a1f3763d020b7cc6121703100af2f057496b1
Ferrardi Dior <146137213+DDTATACOLA@users.noreply.github.com>
2026-09-21 18:26:57 -0500

    Add files via upload

---
+++ 6-sg.tf
@@ -0,0 +1,59 @@
+# resource "aws_security_group" "web" {
+#   name        = "web"
+#   description = "allow HTTP and SSH for web server"
+#   vpc_id      = aws_vpc.agol.id
+
+#   tags = {
+#     Name = "web-ingress"
+#   }
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "http" {
+#   security_group_id = aws_security_group.web.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 80
+#   ip_protocol       = "tcp"
+#   to_port           = 80
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "ssh" {
+#   security_group_id = aws_security_group.web.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 22
+#   ip_protocol       = "tcp"
+#   to_port           = 22
+# }
+
+# resource "aws_vpc_security_group_egress_rule" "egress" {
+#   security_group_id = aws_security_group.web.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# } 
+
+# #************** SG for ping **************
+
+# resource "aws_security_group" "ping" {
+#   name        = "ping"
+#   description = "Allow icmp for ping"
+#   vpc_id      = aws_vpc.agol.id
+
+
+
+#   tags = {
+#     Name = "icmp-for-ping"
+#   }
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "icmp" {
+#   security_group_id = aws_security_group.ping.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = -1
+#   ip_protocol       = "icmp"
+#   to_port           = -1
+# }
+
+# resource "aws_vpc_security_group_egress_rule" "egress_for_ping" {
+#   security_group_id = aws_security_group.ping.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# } 
